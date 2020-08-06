6346456b5c477e2da4f4cd8a63f8a18bbc151bc0
Rafa Zoldan <zoldan.rafael@gmail.com>
2020-08-06 22:55:39 +0300

    rate limit

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const slotRoutes = require('./routes/slots');
-
+const rateLimit = require("express-rate-limit");
+console.log(process.env.RATELIMIT);
+ 
+const limiter = rateLimit({
+  windowMs: 1000 * 10 * 100, 
+  max: process.env.RATELIMIT || 10,
+  message: {
+    error: "Too many requests"
+  }
+});
+ 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-   console.log(`CALL: ${req.method} - ${req.url}`);
-   next();
+    console.log(`CALL: ${req.method} - ${req.url}`);
+    next();
 });
+
+app.use(limiter);
 
 slotRoutes(app);
 
