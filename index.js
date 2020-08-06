c7e02e97aa57fc10d0899388e236546351ee7f7e
Rafa Zoldan <zoldan.rafael@gmail.com>
2020-08-06 13:58:32 +0300

    basic structure

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+   console.log(`CALL: ${req.method} - ${req.url}`);
+   next();
+});
+
+// add my routes here
+
+app.listen(3000, () => {
+    console.log('server is up')
+});
